6d4a99d083be33587678cec918aad287c2b5ec4e
Your Name <kelvin.lightner@gmail.com>
2016-12-18 13:52:14 -0800

    Updates Functionality to Added to Attribute Creation Form

--- barID/src/actions/types.js
+++ barID/src/actions/types.js
@@ -7,3 +7,5 @@ export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 
 export const LOGIN_USER = 'login_user';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
