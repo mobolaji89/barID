d8f101bc0d452c151bd19a20775e1fb848e36c02
Your Name <kelvin.lightner@gmail.com>
2016-12-17 22:16:54 -0800

    Login Form Added

--- barID/src/components/LoginForm.js
+++ barID/src/components/LoginForm.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
+import { Text } from 'react-native';
 
 class LoginForm extends Component {
 	render() {
 		return (
 			<Card>
+				<CardSection>
+					<Input
+						label="Email"
+						placeholder="example@gmail.com"
+					/>
+				</CardSection>
 
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Password"
+						placeholder="password"
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Login
+					</Button>
+				</CardSection>
 			</Card>
 		)
 	}
