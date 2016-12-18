be33b2dde1b5b862b4f983f94beb3bd3ee4b8943
Your Name <kelvin.lightner@gmail.com>
2016-12-18 14:25:17 -0800

    Final Picker Styling

--- barID/src/components/EmployeeCreate.js
+++ barID/src/components/EmployeeCreate.js
@@ -29,7 +29,6 @@ class EmployeeCreate extends Component {
         <CardSection style={{ flexDirection: 'column' }}>
           <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
-            style={{ flex: 1 }}
             selectedValue={this.props.shift}
             onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
           >
