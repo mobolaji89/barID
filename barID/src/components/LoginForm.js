df4beeea0679a1b010c4517580aefb33fad0f4b1
Your Name <kelvin.lightner@gmail.com>
2016-12-17 23:01:41 -0800

    Building Redux into Login Form

--- barID/src/components/LoginForm.js
+++ barID/src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
-import { Text } from 'react-native';
+
 
 class LoginForm extends Component {
+	onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
 	render() {
 		return (
 			<Card>
@@ -10,6 +16,7 @@ class LoginForm extends Component {
 					<Input
 						label="Email"
 						placeholder="example@gmail.com"
+						onChangeText={this.onEmailChange.bind(this)}
 					/>
 				</CardSection>
 
@@ -27,8 +34,8 @@ class LoginForm extends Component {
 					</Button>
 				</CardSection>
 			</Card>
-		)
+		);
 	}
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
