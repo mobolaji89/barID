f92d2df7fda3d26fd42ddaf90d683de287a156c4
Your Name <kelvin.lightner@gmail.com>
2016-12-18 16:54:21 -0800

    Finalize Form Layout by Clearing Data When Necessary

--- barID/src/actions/types.js
+++ barID/src/actions/types.js
@@ -13,3 +13,5 @@ export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = 'employee_create';
 
 export const EMPLOYEES_FETCH_SUCCESS = 'employees_fetch_success';
+
+export const EMPLOYEE_SAVE_SUCCESS = 'employee_save_success';
