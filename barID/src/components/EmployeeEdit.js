d54596590fad71bc8a435ebdfc0b7552f138613a
Your Name <kelvin.lightner@gmail.com>
2016-12-18 17:15:11 -0800

    Can Delete and Attribute from Firebase -- CORE APP COMPLETE

--- barID/src/components/EmployeeEdit.js
+++ barID/src/components/EmployeeEdit.js
@@ -1,9 +1,10 @@
+
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -25,6 +26,16 @@ class EmployeeEdit extends Component {
     const { phone, shift } = this.props;
 
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onAccept() {
+    const { uid } = this.props.employee;
+
+    this.props.employeeDelete({ uid });
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
   }
 
   render() {
@@ -52,6 +63,8 @@ class EmployeeEdit extends Component {
 
         <Confirm
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </Confirm>
@@ -67,5 +80,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeSave
+  employeeUpdate, employeeSave, employeeDelete
 })(EmployeeEdit);
